a4c2be7a5348f2743dcdc7f792b2a741dd016dc1
djx314 <djx314@sina.cn>
2018-04-15 01:49:05 +0800

    Add string test.

--- src/main/scala/net/scalax/cpoi/package.scala
+++ src/main/scala/net/scalax/cpoi/package.scala
@@ -1,10 +1,13 @@
 package net.scalax
 
+import net.scalax.cpoi.exception.CellReaderException
 import net.scalax.cpoi.rw.{CellReadersImplicits, CellWritersImplicits}
 
 package object cpoi {
 
   val readers: CellReadersImplicits = new CellReadersImplicits {}
   val writers: CellWritersImplicits = new CellWritersImplicits {}
+
+  type CellReadResult[R] = Either[CellReaderException, R]
 
 }
