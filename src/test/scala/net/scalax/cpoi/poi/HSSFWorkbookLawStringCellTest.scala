5fea7c2f381caf36ba20085b320b30fa4e87b033
djx314 <djx314@sina.cn>
2018-04-17 01:11:10 +0800

    Add law poi test for take care of when the common poi future changed.

---
+++ src/test/scala/net/scalax/cpoi/poi/HSSFWorkbookLawStringCellTest.scala
@@ -0,0 +1,43 @@
+package net.scalax.cpoi
+
+import java.util.Date
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import org.apache.poi.ss.usermodel.{Cell, CellType}
+import org.scalatest._
+
+class HSSFWorkbookLawStringCellTest extends FlatSpec with Matchers {
+
+  "Numberic cell" should "be set a string cell type" in {
+    val workbook = new HSSFWorkbook()
+    val sheet = workbook.createSheet("Sheet1")
+    val cell = sheet.createRow(0).createCell(0)
+    cell.setCellValue(2333.2233)
+    cell.setCellType(CellType.STRING)
+    cell.getStringCellValue should be("2333.2233")
+  }
+
+  "Boolean cell" should "be set a string cell type" in {
+    val workbook = new HSSFWorkbook()
+    val sheet = workbook.createSheet("Sheet1")
+    val cell = sheet.createRow(0).createCell(0)
+    cell.setCellValue(true)
+    cell.setCellType(CellType.STRING)
+    cell.getStringCellValue should be("TRUE")
+
+    val cell2 = sheet.createRow(0).createCell(2)
+    cell2.setCellValue(false)
+    cell2.setCellType(CellType.STRING)
+    cell2.getStringCellValue should be("FALSE")
+  }
+
+  "Date cell" should "be set a string cell type" in {
+    val workbook = new HSSFWorkbook()
+    val sheet = workbook.createSheet("Sheet1")
+    val cell = sheet.createRow(0).createCell(0)
+    cell.setCellValue(new Date(0))
+    cell.setCellType(CellType.STRING)
+    cell.getStringCellValue should be("25569.3333333333")
+  }
+
+}
--- src/test/scala/net/scalax/cpoi/read/HSSFWorkbookFileStringTest.scala
+++ src/test/scala/net/scalax/cpoi/read/HSSFWorkbookFileStringTest.scala
@@ -1,27 +1,104 @@
 package net.scalax.cpoi
 
+import java.util.Date
+
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
 import org.apache.poi.ss.usermodel.{Cell, CellType}
 import org.scalatest._
 
 class HSSFWorkbookFileStringTest extends FlatSpec with Matchers {
 
-  "CellContent" should "throw exception when read an empty cell as string" in {
-    val workbook = new HSSFWorkbook(getClass.getClassLoader.getResourceAsStream("test01.xls"))
-    val sheet = workbook.getSheet("Sheet1")
-    println("11" * 100)
+  "String cell" should "throw exception when setting a numeric cell type" in {
+    val workbook = new HSSFWorkbook()
+    val sheet = workbook.createSheet("Sheet1")
+    val cell = sheet.createRow(0).createCell(0)
+    cell.setCellValue("2333.44")
+    a[IllegalStateException] should be thrownBy {
+      cell.setCellType(CellType.NUMERIC)
+    }
+  }
 
-    val cell = sheet.getRow(6).getCell(0)
+  "String cell" should "be set a boolean cell type" in {
+    val workbook = new HSSFWorkbook()
+    val sheet = workbook.createSheet("Sheet1")
+    val cell = sheet.createRow(0).createCell(0)
+    cell.setCellValue("2333.44")
+    cell.setCellType(CellType.BOOLEAN)
+    cell.getBooleanCellValue should be(false)
+  }
 
-    val cloneCell1 = cell.clone().asInstanceOf[Cell]
+  "Numberic cell" should "be set a string cell type" in {
+    val workbook = new HSSFWorkbook()
+    val sheet = workbook.createSheet("Sheet1")
+    val cell = sheet.createRow(0).createCell(0)
+    cell.setCellValue(2333.2233)
+    cell.setCellType(CellType.STRING)
+    cell.getStringCellValue should be("2333.2233")
+  }
 
-    cloneCell1.setCellType(CellType.STRING)
-    println(cloneCell1.getStringCellValue)
+  "Numberic cell" should "be set a boolean cell type" in {
+    val workbook = new HSSFWorkbook()
+    val sheet = workbook.createSheet("Sheet1")
+    val cell = sheet.createRow(0).createCell(0)
+    cell.setCellValue(2333.2233)
+    cell.setCellType(CellType.BOOLEAN)
+    cell.getBooleanCellValue should be(true)
 
-    val cloneCell12 = cell.clone().asInstanceOf[Cell]
+    val cell2 = sheet.createRow(0).createCell(0)
+    cell2.setCellValue(-2333.2233)
+    cell2.setCellType(CellType.BOOLEAN)
+    cell2.getBooleanCellValue should be(true)
+  }
 
-    cloneCell12.setCellType(CellType.NUMERIC)
-    println(cloneCell12.getDateCellValue)
+  "Boolean cell" should "be set a string cell type" in {
+    val workbook = new HSSFWorkbook()
+    val sheet = workbook.createSheet("Sheet1")
+    val cell = sheet.createRow(0).createCell(0)
+    cell.setCellValue(true)
+    cell.setCellType(CellType.STRING)
+    cell.getStringCellValue should be("TRUE")
+
+    val cell2 = sheet.createRow(0).createCell(2)
+    cell2.setCellValue(false)
+    cell2.setCellType(CellType.STRING)
+    cell2.getStringCellValue should be("FALSE")
+  }
+
+  "Boolean cell" should "be set a numberic cell type" in {
+    val workbook = new HSSFWorkbook()
+    val sheet = workbook.createSheet("Sheet1")
+    val cell = sheet.createRow(0).createCell(0)
+    cell.setCellValue(false)
+    a[IllegalStateException] should be thrownBy {
+      cell.setCellType(CellType.NUMERIC)
+    }
+  }
+
+  "Date cell" should "be set a boolean cell type" in {
+    val workbook = new HSSFWorkbook()
+    val sheet = workbook.createSheet("Sheet1")
+    val cell = sheet.createRow(0).createCell(0)
+    cell.setCellValue(new Date())
+    cell.setCellType(CellType.BOOLEAN)
+    cell.getBooleanCellValue should be(true)
+  }
+
+  "Date cell" should "be set a string cell type" in {
+    val workbook = new HSSFWorkbook()
+    val sheet = workbook.createSheet("Sheet1")
+    val cell = sheet.createRow(0).createCell(0)
+    cell.setCellValue(new Date(0))
+    cell.setCellType(CellType.STRING)
+    cell.getStringCellValue should be("25569.3333333333")
+  }
+
+  "Date cell" should "be set a numeric cell type" in {
+    val workbook = new HSSFWorkbook()
+    val sheet = workbook.createSheet("Sheet1")
+    val cell = sheet.createRow(0).createCell(0)
+    cell.setCellValue(new Date(0))
+    cell.setCellType(CellType.NUMERIC)
+    cell.getNumericCellValue should be(25569.333333333332d)
   }
 
 }
