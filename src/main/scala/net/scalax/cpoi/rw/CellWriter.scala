a3cf5133b6b703cdf14e32f5a88e49f007cb505b
djx314 <djx314@sina.cn>
2018-04-17 05:08:35 +0800

    Add contravariant for writer.

--- src/main/scala/net/scalax/cpoi/rw/CellWriter.scala
+++ src/main/scala/net/scalax/cpoi/rw/CellWriter.scala
@@ -1,9 +1,12 @@
 package net.scalax.cpoi.rw
 
+import cats.Contravariant
 import org.apache.poi.ss.usermodel.Cell
 
+import scala.util.Try
+
 trait CellWriter[T] {
-  def setValue(cell: Cell, value: T): Boolean
+  def setValue(cell: Cell, value: T): Try[Boolean]
 }
 
 object CellWriter {
@@ -11,15 +14,26 @@ object CellWriter {
   implicit def optionCellOperationToNoneOptionCellOpreation[T: CellWriter]
     : CellWriter[Option[T]] = {
     new CellWriter[Option[T]] {
-
-      override def setValue(cell: Cell, value: Option[T]): Boolean = {
+      override def setValue(cell: Cell, value: Option[T]): Try[Boolean] = {
         value
           .map { v =>
             implicitly[CellWriter[T]].setValue(cell, v)
           }
-          .getOrElse(true)
+          .getOrElse(Try(true))
       }
+    }
+  }
 
+  implicit val contravariant
+  : Contravariant[CellWriter] = {
+    new Contravariant[CellWriter] {
+      override def contramap[A, B](fa: CellWriter[A])(f: B => A): CellWriter[B] = {
+        new CellWriter[B] {
+          override def setValue(cell: Cell, value: B): Try[Boolean] = {
+            fa.setValue(cell, f(value))
+          }
+        }
+      }
     }
   }
 
